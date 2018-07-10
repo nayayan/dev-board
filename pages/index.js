f092591f66b7618873eddc028797a3cd555f711a
nayayan <yan.bbouchard@gmail.com>
2018-07-10 17:23:45 -0400

    Added dnd-board

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import Head from "next/head";
 
 import Global from "./_global.js";
 import Navigation from "../components/navigation";
-import env from "../env.config.js";
+import Board from "../components/board";
 import styles from "./index.scss";
 
 export default class extends React.Component {
@@ -13,9 +13,7 @@ export default class extends React.Component {
         <Head>
           <title>Home</title>
         </Head>
-        <Navigation />
-        <h1>Welcome to wild-next!</h1>
-        <pre>env.config.js</pre><p>says you defined <em>{env.PUBLIC_URL}</em> as the public url for this environment.</p>
+        <Board/>
         <style jsx>{styles}</style>
       </Global>
     );
